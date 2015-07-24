175d3755895f63cbc194ac34b2f5bd77685d989e
V3494 (Conference ID) <v3494@cam.ac.uk>
2015-07-24 11:58:25 +0100

    sup brah

--- drone-backend.js
+++ drone-backend.js
@@ -21,42 +21,45 @@ function fly(robot) {
     bot.drone.ftrim();
     bot.drone.takeoff();
     after(0*1000, function(){
-        bot.drone.up(0.1);
+        bot.drone.up(1);
     });
     after(1*1000, function(){
+        bot.drone.up(0);
+    })
+    after(5*1000, function(){
         bot.drone.left(0.1);
     });
-    after(2*1000, function(){ //this is too stop the drone moving before a further command is given
-        bot.drone.right(0);
-    });
-    after(2*1000, function(){
-        bot.drone.front(0.1);
-    });
-    after(4*1000, function(){//this is too stop the drone moving before a further command is given
-        bot.drone.back(0);
-    });
-    after(4*1000, function(){
-        bot.drone.right(0.1);
-    });
-    after(6*1000, function(){//this is too stop the drone moving before a further command is given
+    after(6*1000, function(){ //this is too stop the drone moving before a further command is given
         bot.drone.left(0);
     });
     after(6*1000, function(){
-        bot.drone.back(0.1);
+        bot.drone.front(0.1);
     });
     after(8*1000, function(){//this is too stop the drone moving before a further command is given
-        bot.drone.forward(0);
+        bot.drone.front(0);
     });
     after(8*1000, function(){
-        bot.drone.left(0.1);
+        bot.drone.right(0.1);
     });
-    after(9*1000, function(){//this is too stop the drone moving before a further command is given
+    after(10*1000, function(){//this is too stop the drone moving before a further command is given
         bot.drone.right(0);
     });
-    after(9*1000, function(){
+    after(10*1000, function(){
+        bot.drone.back(0.1);
+    });
+    after(12*1000, function(){//this is too stop the drone moving before a further command is given
+        bot.drone.back(0);
+    });
+    after(12*1000, function(){
+        bot.drone.left(0.1);
+    });
+    after(13*1000, function(){//this is too stop the drone moving before a further command is given
+        bot.drone.left(0);
+    });
+    after(13*1000, function(){
         bot.drone.down(0.1);
     });
-    after(10*1000, function(){
+    after(20*1000, function(){
         bot.drone.land();
     });
 }
